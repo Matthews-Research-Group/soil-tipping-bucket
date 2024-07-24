8e76b6ffceb1d7ca62a97aaf42386c9a46c2f3fa
uphone87 <yufenghe87@gmail.com>
2024-07-24 11:35:14 -0500

    some updates

--- run/full_BioCro_run.R
+++ run/full_BioCro_run.R
@@ -1,46 +1,73 @@
-set_direct_modules<-function(){
+library(BioCro)
+library(BioCroWater)
+years = 2004:2011
+obs_data = readRDS("data/soil_moisture_data_rearranged.rds")
+obs_data$sm = obs_data$sm/100
+#obs depth defition:
+#layer 1: 5-15 cm; layer 2: 15-25; layer 3: 25-35.... layer 7: 65-75 cm
+results = list()
+ctl = list()
+for (i in 1:length(years)){
+  year = years[i]
+  weatherData <- 
+    read.csv(paste0("WeatherData/",year,"/",year,"_Bondville_IL_daylength.csv"))
+  #further subset from the start of obs DOY
+  weather_growing_season = weatherData[weatherData$doy>=150 & weatherData$doy<=280,]
+  weather_growing_season$time_zone_offset = -6
+  
+  #get obs data
+  obs_year_i = obs_data[obs_data$year==year,]
+  obs_avg = list()
+  for (depth in 1:7){
+    obs = obs_year_i[obs_year_i$depth==depth,] 
+    obs = obs[,c('ring','doy','sm')]
+    tmp = aggregate(sm ~ doy, data = obs, FUN = mean)
+    obs_avg[[depth]] = tmp
+  }
+  
+  ctl[[i]] <- run_biocro(
+    soybean$initial_values,
+    soybean$parameters,
+    weather_growing_season,
+    soybean$direct_modules,
+    soybean$differential_modules,
+    soybean$ode_solver
+  )
+  
+  soiltype = 3
   direct_modules_soil_water = list("BioCroWater:soil_type_selector", "BioCroWater:soil_surface_runoff",
                                     "BioCroWater:soil_water_downflow", "BioCroWater:soil_water_tiledrain", 
                                     "BioCroWater:soil_water_upflow","BioCroWater:soil_water_uptake",
                                     "BioCroWater:multilayer_soil_profile_avg")
+  
+  differential_modules_soil_water = list("BioCroWater:soil_evaporation","BioCroWater:multi_layer_soil_profile")
+  
   direct_modules_new = soybean$direct_modules
   old_soil_evapo_index = which(direct_modules_new=="BioCro:soil_evaporation")
   direct_modules_new = direct_modules_new[-old_soil_evapo_index] #remove soil evaporation
   direct_modules_new = c(direct_modules_new[1:(old_soil_evapo_index-1)],direct_modules_soil_water,
                          direct_modules_new[old_soil_evapo_index:length(direct_modules_new)]) # insert BioCroWater
-  return(direct_modules_new)
-}
   
-set_differential_modules<-function(){
-  differential_modules_soil_water = list("BioCroWater:soil_evaporation","BioCroWater:multi_layer_soil_profile")
   differential_modules_new = soybean$differential_modules
   old_soil_profile_index = which(differential_modules_new=="BioCro:two_layer_soil_profile")
   differential_modules_new = differential_modules_new[-old_soil_profile_index] #remove soil_profile
   differential_modules_new = c(differential_modules_new[1:(old_soil_profile_index-1)],
                                differential_modules_soil_water,
                                differential_modules_new[old_soil_profile_index:length(differential_modules_new)])
-  return(differential_modules_new)
-}
-
-set_init_values<-function(){
+  
   init_values =   within(soybean$initial_values,{
-    soil_water_content_1 = soybean$initial_values$soil_water_content  #0-5cm
-    soil_water_content_2 = soybean$initial_values$soil_water_content  #5-15cm
-    soil_water_content_3 = soybean$initial_values$soil_water_content  #15-35cm
-    soil_water_content_4 = soybean$initial_values$soil_water_content  #35-55 cm
-    soil_water_content_5 = soybean$initial_values$soil_water_content  #55-75 cm
-    soil_water_content_6 = soybean$initial_values$soil_water_content  #75-100cm
+    soil_water_content_1 = obs_avg[[1]]$sm[1]  #0-5cm
+    soil_water_content_2 = obs_avg[[1]]$sm[1]  #5-15cm
+    soil_water_content_3 = obs_avg[[2]]$sm[1]  #15-35cm
+    soil_water_content_4 = obs_avg[[4]]$sm[1]  #35-55 cm
+    soil_water_content_5 = obs_avg[[6]]$sm[1]  #55-75 cm
+    soil_water_content_6 = obs_avg[[7]]$sm[1]  #75-100cm
     sumes1               = 0.125
     sumes2               = 0
     time_factor = 0
     soil_evaporation_rate = 0
   })
   init_values$soil_water_content=NULL
-  return(init_values)
-}
-
-set_parameters<-function(){
-  soiltype = 3 
   parameters =   within(soybean$parameters, {
     irrigation = 0.0
     swcon = 0.05/24
@@ -67,5 +94,22 @@ set_parameters<-function(){
   })
   parameters[c('soil_field_capacity','soil_saturated_conductivity','soil_saturation_capacity','soil_wilting_point')]=NULL
   parameters[c('kShell','net_assimilation_rate_shell')] = NULL
-  return(parameters)
+  
+  results[[i]] <- run_biocro(
+    init_values,
+    parameters,
+    weather_growing_season,
+    direct_modules_new,
+    differential_modules_new
+  )
 }
+
+i=1
+plot(results[[i]]$time,results[[i]]$Grain,type='l',ylim=c(0,8))
+lines(ctl[[i]]$time,ctl[[i]]$Grain,col='red')
+
+plot(results[[i]]$time,results[[i]]$lai,type='l')
+lines(ctl[[i]]$time,ctl[[i]]$lai,col='red')
+
+plot(results[[i]]$time,results[[i]]$soil_water_content,type='l',ylim=c(0.2,0.6))
+lines(ctl[[i]]$time,ctl[[i]]$soil_water_content,col='red')
--- run/optimization/plot_optimization.R
+++ run/optimization/plot_optimization.R
@@ -1,21 +1,23 @@
 library(BioCro)
+library(BioCroWater)
 library(ggplot2) # To ggplot functions
 library(reshape2) # For melt function
 library(data.table) # For first and last functions
 library(gridExtra)
 
+source('set_modules_for_soil_water.R')
+source('plot_funcs.R')
 #only need to change this name. 
-opt_result_name = 'withseed_newBioCro_r15'
+opt_result_name = 'control'
 #
 # Cost function
-run_cwrfsoilwater = FALSE
-arg_names <- c('alphaLeaf','alphaRoot','alphaStem','betaLeaf','betaRoot','betaStem',
-               'rateSeneLeaf','rateSeneStem','alphaSeneLeaf','betaSeneLeaf',
-               'alphaSeneStem','betaSeneStem','alphaShell','betaShell') 
-new_par = readRDS(paste0('opt_results/optim_result_DEoptim_',opt_result_name,'.rds'))
-new_par = new_par$optim$bestmem
-names(new_par) = arg_names
-print(cbind(arg_names,new_par))
+#arg_names <- c('alphaLeaf','alphaRoot','alphaStem','betaLeaf','betaRoot','betaStem',
+#               'rateSeneLeaf','rateSeneStem','alphaSeneLeaf','betaSeneLeaf',
+#               'alphaSeneStem','betaSeneStem','alphaShell','betaShell') 
+#new_par = readRDS(paste0('opt_results/optim_result_DEoptim_',opt_result_name,'.rds'))
+#new_par = new_par$optim$bestmem
+#names(new_par) = arg_names
+#print(cbind(arg_names,new_par))
 #soybean_parameters0          = soybean$parameters
 #print(cbind(new_par,r=soybean_parameters0[arg_names]))
 
@@ -26,7 +28,8 @@ dates <- data.frame("year" = c(2002, 2004:2006),"sow" = c(152,149,148,148), "har
 
 # initialize variables
 results <- list()
-weather.growingseason <- list()
+results_CTL <- list()
+weather_growing_season <- list()
 ExpBiomass <- list()
 ExpBiomass.std <- list()
 soybean_solver <- list()
@@ -34,16 +37,11 @@ RootVals <- list()
 weights <- list()
 numrows <- vector()
 
-soybean_steadystate_modules0 = soybean$direct_modules 
-soybean_derivative_modules0  = soybean$differential_modules  
-soybean_initial_state0       = soybean$initial_values 
-soybean_initial_state0$Shell   = 1e-5 
-soybean_parameters0            = soybean$parameters
-soybean_parameters0$time_zone_offset= -6
-
-if(run_cwrfsoilwater){
-  soybean_initial_state0 = soybean_initial_state0[names(soybean_initial_state0)!='soil_water_content']
-}
+soybean_steadystate_modules0 = set_direct_modules() 
+soybean_derivative_modules0  = set_differential_modules() 
+soybean_initial_state0       = set_init_values() 
+soybean_parameters0          = set_parameters() 
+soybean_parameters0$kd = soybean_parameters0$k_diffuse
 
 soybean_solver_params=soybean$ode_solver
 soybean_solver_params$type="homemade_euler" #comment this out to use rkck54 solver
@@ -51,28 +49,40 @@ soybean_solver_params$type="homemade_euler" #comment this out to use rkck54 solv
 for (i in 1:length(years)) {
   yr <- years[i]
   weather <- read.csv(file = paste0('Data/Weather_data/', yr,'_Bondville_IL_daylength.csv'))
-  cwrf_soil <- read.csv(file = paste0('soilwater_Urbana/cwrf_soilwater_', yr,'.csv'))
-  if(run_cwrfsoilwater){
-    weather = cbind(weather,cwrf_soil[,c(4,5)])
-    colnames(weather)[colnames(weather)=="swc"]="soil_water_content" 
-    soybean_parameters0$soil_type_indicator = 5#weather$soiltype[1]
-  }
+
   sowdate <- dates$sow[which(dates$year == yr)]
   harvestdate <- dates$harvest[which(dates$year == yr)]
   sd.ind <- which(weather$doy == sowdate)[1]
   hd.ind <- which(weather$doy == harvestdate)[24]
   
-  weather.growingseason <- weather[sd.ind:hd.ind,]
+  weather_growing_season <- weather[sd.ind:hd.ind,]
+  weather_growing_season$time_zone_offset = -6
+
+#calculate CTL for diagnostic purposes
+  results_CTL[[i]] <- run_biocro(
+    soybean$initial_values,
+    soybean$parameters,
+    weather_growing_season,
+    soybean$direct_modules,
+    soybean$differential_modules,
+    soybean$ode_solver
+  )
   
-  soybean_solver[[i]] <- partial_run_biocro(soybean_initial_state0, 
- 					       soybean_parameters0, 
-					       weather.growingseason,
-                                               soybean_steadystate_modules0,
-					       soybean_derivative_modules0, 
-                                               soybean_solver_params,arg_names)
-  
-  soybean_solver_i = soybean_solver[[i]] 
-  results[[i]] <- soybean_solver_i(new_par)
+#  soybean_solver[[i]] <- partial_run_biocro(soybean_initial_state0, 
+# 					       soybean_parameters0, 
+#					       weather_growing_season,
+#                                               soybean_steadystate_modules0,
+#					       soybean_derivative_modules0, 
+#                                               soybean_solver_params,arg_names)
+#  
+#  soybean_solver_i = soybean_solver[[i]] 
+#  results[[i]] <- soybean_solver_i(new_par)
+  results[[i]] <- run_biocro(soybean_initial_state0, 
+ 			                       soybean_parameters0, 
+			                       weather_growing_season,
+                             soybean_steadystate_modules0,
+			                       soybean_derivative_modules0, 
+                             soybean_solver_params)
 
   ExpBiomass[[i]] <- read.csv(file=paste0('Data/biomasses_with_seed/',yr,'_ambient_biomass.csv'))
   colnames(ExpBiomass[[i]])<-c("DOY","Leaf","Stem","Shell0","Seed")
@@ -87,61 +97,20 @@ for (i in 1:length(years)) {
   
   RootVals[[i]] <- data.frame("DOY"=ExpBiomass[[i]]$DOY[5], "Root"=0.17*sum(ExpBiomass[[i]][5,2:4])) # See Ordonez et al. 2020, https://doi.org/10.1016/j.eja.2020.126130
   
-  numrows[i] <- nrow(weather.growingseason)
+  numrows[i] <- nrow(weather_growing_season)
   invwts <- ExpBiomass.std[[i]]
   weights[[i]] <- log(1/(invwts[,2:ncol(invwts)]+1e-5))
 
 }
 
-# Define functions to create plots
-plot_all_tissues <- function(res, year, biomass, biomass.std) {
-  
-  r <- reshape2::melt(res[, c("time","Root","Leaf","Stem","Shell","Grain")], id.vars="time")
-  r.exp <- reshape2::melt(biomass[, c("DOY", "Leaf", "Stem", "Shell","Seed")], id.vars = "DOY")
-  r.exp.std <- reshape2::melt(biomass.std[, c("DOY", "Leaf", "Stem", "Shell","Seed")], id.vars = "DOY")
-  r.exp.std$ymin<-r.exp$value-r.exp.std$value
-  r.exp.std$ymax<-r.exp$value+r.exp.std$value
-  
-  # Colorblind friendly color palette (https://personal.sron.nl/~pault/)
-  col.palette.muted <- c("#332288", "#117733", "#999933", "#882255","#EE3377")
-  
-  size.title <- 12
-  size.axislabel <-10
-  size.axis <- 10
-  size.legend <- 12
-  
-  f <- ggplot() + theme_classic()
-  f <- f + geom_point(data=r, aes(x=time,y=value, colour=variable), show.legend = TRUE, size=0.25)
-  f <- f + geom_errorbar(data=r.exp.std, aes(x=DOY, ymin=ymin, ymax=ymax), width=3.5, size=.25, show.legend = FALSE)
-  f <- f + geom_point(data=r.exp, aes(x=DOY, y=value, fill=variable), shape=22, size=2, show.legend = FALSE, stroke=.5)
-  f <- f + labs(title=element_blank(), x=paste0('Day of Year (',year,')'),y='Biomass (Mg / ha)')
-  f <- f + coord_cartesian(ylim = c(0,10)) + scale_y_continuous(breaks = seq(0,10,2)) + scale_x_continuous(breaks = seq(150,275,30))
-  f <- f + theme(plot.title=element_text(size=size.title, hjust=0.5),
-                 axis.text=element_text(size=size.axis),
-                 axis.title=element_text(size=size.axislabel),
-                 legend.position = c(.15,.85), legend.title = element_blank(),
-                 legend.text=element_text(size=size.legend),
-                 legend.background = element_rect(fill = "transparent",colour = NA),
-                 panel.grid.major = element_blank(),
-                 panel.grid.minor = element_blank(), panel.background = element_rect(fill = "transparent",colour = NA),
-                 plot.background = element_rect(fill = "transparent", colour = NA))
-  f <- f + guides(colour = guide_legend(override.aes = list(size=2)))
-  f <- f + scale_fill_manual(values = col.palette.muted[2:5], guide = FALSE)
-  f <- f + scale_colour_manual(values = col.palette.muted, labels=c('Root','Leaf','Stem','Shell','Seed'))
-  
-  return(f)
-}
-
-
 plot_list = list()
 for (i in 1:length(years)){
 	yr = years[i]
-	FigA <- plot_all_tissues(results[[i]], yr, ExpBiomass[[i]], ExpBiomass.std[[i]])
+	FigA <- plot_all_tissues_TWO(results_CTL[[i]],results[[i]], yr, ExpBiomass[[i]], ExpBiomass.std[[i]])
 	plot_list[[i]] = FigA
 }
-pdf_name = paste0("figs/fig_",opt_result_name,"_Rdirect.pdf")
+pdf_name = paste0("figs/fig_",opt_result_name,".pdf")
 pdf(pdf_name,height = 8, width=8,bg='transparent')
 grid.arrange(grobs = plot_list,nrow=2,ncol=2)
 dev.off()
 
-#saveRDS(results,paste0('result_',opt_result_name,'.rds'))
